7826a6f5a07e2f4fab83e1db7f7dc2d15286d855
Federico <federico.valeri@studenti.unicam.it>
2021-10-08 21:43:49 +0200

    started the provide_reccommendation function

--- execute_tests.py
+++ execute_tests.py
@@ -7,13 +7,18 @@ import requests
 import shutil
 import configparser
 import csv
+import sys
 import json
+import yaml
+import pyaml
+import math
 import datetime
 import threading
 import pandas as pd
 from threading import Thread
 from threading import Timer
 from metrics.Metric import Metrics
+from prettytable import PrettyTable
 from lib import perform_test
 
 
@@ -104,12 +109,6 @@ def get_metric(metric, configuration, section, container_name):
     f.write(metric_value + '\n')
     f.close()
 
-# def provide_reccomendations():
-#     f2 = open("./metrics/monitored_cpu.txt", "a")
-#     f2.write(max_metric_value + '\n')
-#     #f2.write(str(average_metric_value) + '\n')
-#     f2.close()
-
 
 def execute_tests(configuration):
     create_test_plan()
@@ -126,32 +125,99 @@ def execute_tests(configuration):
     # run_plugins(configuration, "DEFAULT", design_path, None, "teardown")
 
 
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "Ki", "Mi", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p)
+    return "%s%s" % (s, size_name[i])
+
+
+def provide_reccomendations(metrics, configuration, section, container_name):
+    target_cpu = 0
+    target_memory = 0
+    cpu_request = 0
+    memory_request = 0
+    query = ""
+    prometheus_url = configuration[section]["PROMETHEUS_HOST_URL"]
+    for metric in metrics:
+        lines = []
+        query = "avg(kube_pod_container_resource_requests{resource='" + \
+                metric + "', container='" + container_name + "'})"
+        response = requests.get(
+            prometheus_url + '/api/v1/query', params={'query': query})
+        results = response.json()['data']['result']
+        with open(f'./metrics/monitored_{metric}.txt') as f:
+            lines = f.readlines()
+        target_metric = max(lines)
+        if(metric == Metrics().CPU):
+            target_cpu = int(float(target_metric) * 1000)
+            for result in results:
+                value = ('{value[1]}'.format(**result))
+                cpu_request = int(float(value) * 1000)
+        if(metric == Metrics().MEMORY):
+            target_memory = convert_size(int(target_metric))
+            for result in results:
+                value = ('{value[1]}'.format(**result))
+                memory_request = convert_size(int(value))
+
+    target_cpu = str(target_cpu) + 'm'
+    # creating an empty PrettyTable
+    ascii_table = PrettyTable()
+
+    # adding data into the table column by column
+    ascii_table.add_column("Metrics", metrics)
+    ascii_table.add_column("Actual requests", [str(
+        cpu_request) + 'm', memory_request])
+    ascii_table.add_column("Target", [target_cpu, target_memory])
+
+    # printing table
+    print(ascii_table)
+
+    yaml_output = dict(
+        resources=dict(
+            requests=dict(
+                cpu=target_cpu,
+                memory=target_memory
+            )
+        )
+    )
+
+    with open('test.yaml', 'w') as outfile:
+        yaml.dump(yaml_output, outfile, default_flow_style=False)
+
+    yaml.safe_dump(yaml_output, sys.stdout)
+
+
 # if __name__ == "__main__":
 #     design_path = './config/'
 #     configuration = configparser.ConfigParser()
 #     configuration.read([os.path.join(
 #         design_path, "configuration.ini"), os.path.join(design_path, "test_plan.ini")])
 #     execute_tests(configuration)
-
 ########## THREADING ###########
-
 if __name__ == "__main__":
-    create_test_plan()
+    # create_test_plan()
     design_path = './config/'
     configuration = configparser.ConfigParser()
     configuration.read([os.path.join(
         design_path, "configuration.ini"), os.path.join(design_path, "test_plan.ini")])
-    for section in configuration.sections():
-        if section.lower().startswith("test"):
-            test = Thread(target=perform_test, args=[
-                configuration, section, design_path])
-            monitor_cpu = Thread(target=get_metrics_during_test, args=[
-                "cpu", configuration, "DEFAULT", "carts"])
-            monitor_memory = Thread(target=get_metrics_during_test, args=[
-                "memory", configuration, "DEFAULT", "carts"])
-            test.start()
-            monitor_cpu.start()
-            monitor_memory.start()
-            test.join()
-            monitor_cpu.join()
-            monitor_memory.join()
+    container_name = configuration["DEFAULT"]["CONTAINER_TO_BE_MONIORED"]
+    # for section in configuration.sections():
+    #     if section.lower().startswith("test"):
+    #         test = Thread(target=perform_test, args=[
+    #             configuration, section, design_path])
+    #         monitor_cpu = Thread(target=get_metrics_during_test, args=[
+    #             "cpu", configuration, "DEFAULT", container_name])
+    #         monitor_memory = Thread(target=get_metrics_during_test, args=[
+    #             "memory", configuration, "DEFAULT", container_name])
+    #         test.start()
+    #         monitor_cpu.start()
+    #         monitor_memory.start()
+    #         test.join()
+    #         monitor_cpu.join()
+    #         monitor_memory.join()
+    metrics = ['cpu', 'memory']
+    provide_reccomendations(metrics, configuration, "DEFAULT", container_name)
--- plugins/deploy_app.py
+++ plugins/deploy_app.py
@@ -10,15 +10,15 @@ from lib import run_external_applicaton
 
 def deploy_app(global_plugin_state, current_configuration, output, test_id):
     #seconds_to_wait_for_running_pods = int(current_configuration["MINIKUBE_WAITING_FOR_PODS_RUNNING_IN_SECONDS"])
-    run_external_applicaton('kubectl apply -f ./config/minikube-sock-shop.yaml')
+    run_external_applicaton('kubectl apply -f ./config/deploy/kubernetes/manifest.yaml')
+    run_external_applicaton('kubectl apply -f ./config/deploy/kubernetes/monitoring')
 
 
-def check_status():
+def check_status(namespace):
     # Controllo se tutti i pod sono nello status "Running" programmaticamente
-    app_namespace = 'default'
     config.load_kube_config()
     v1 = client.CoreV1Api()
-    ret = v1.list_namespaced_pod(app_namespace, watch=False)
+    ret = v1.list_namespaced_pod(namespace, watch=False)
     pods = ret.items
     pods_status = [pod.status.phase for pod in pods]
     result = all(status == 'Running' for status in pods_status)
@@ -28,7 +28,7 @@ def check_status():
 def check_pods_status(global_plugin_state, current_configuration, output, test_id):
     with yaspin(Spinners.line, text="Checking pods to be ready. This could take some minutes...") as sp:
         while(True):
-            pods_are_running = check_status()
+            pods_are_running = check_status('sock-shop')
             if(pods_are_running):
                 print("All pods are running!")
                 break
